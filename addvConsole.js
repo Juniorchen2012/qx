d04ef27efa926759788f6ab7e32d70257edc701c
Juniorchen2012 <Juniorchen2012@users.noreply.github.com>
2023-08-29 09:08:08 +0800

    Update addvConsole.js

--- addvConsole.js
+++ addvConsole.js
@@ -1,1 +1,2 @@
-
+var body = $response.body.replace(/<head>/g, "<head><script src='https://unpkg.com/vconsole@latest/dist/vconsole.min.js'></script><script>var vConsole = new window.VConsole();</script>")
+$done({ body })
