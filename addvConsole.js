d928270a9b63e9f406dfc3e0024fe866523b5057
Juniorchen2012 <Juniorchen2012@users.noreply.github.com>
2023-08-29 14:49:30 +0800

    Update addvConsole.js

--- addvConsole.js
+++ addvConsole.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/<head>/g, "<head><script src='https://unpkg.com/vconsole@latest/dist/vconsole.min.js'></script><script>var vConsole = new window.VConsole();</script>")
+var body = $response.body.replace(/<head>/g, "<head><script src='//cdn.jsdelivr.net/npm/eruda'></script><script>eruda.init();</script><script src='https://unpkg.com/vconsole@latest/dist/vconsole.min.js'></script><script>var vConsole = new window.VConsole();</script>")
 $done({ body })
